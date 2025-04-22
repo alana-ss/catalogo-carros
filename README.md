c428f877a18f511f34616c351aee9036b64363bc
alana-ss <nahil.santos@upaep.edu.mx>
2025-04-21 23:07:28 -0600

    Se añadio estructura base de la pagina mas el archivo .css y README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# 🚗 Catálogo de Carros Deportivos
+
+Este proyecto es una página web interactiva que muestra un catálogo de carros deportivos de las marcas Porsche, Corvette y Audi.
+
+El catalogo se va aorganizar de la siguiente manera
+-3 marcas disponibles: Porsche, Corvette, Audi
+-2 modelos por marca que contendran cada uno: nombre, tipo, año, precio, icono
--- carros.js
+++ carros.js
@@ -1,2 +1,66 @@
-let mensaje = "Cree mi primer Git y GitHub"
+let mensaje = "Bienvenido amante de los autos, continua para descubrir lo que tenemos para ti :) 🚗"
 alert(mensaje)
+const marcas= {
+    porsche: {
+        nombre: "Porsche", modelos: [  {
+            nombre: "911 Turbo S",
+            tipo: "Automático",
+            año: 2025,
+            precio: "$2,710,000",
+            icono: "⚡"
+        },
+        {
+            nombre: "911 gt3 rs",
+            tipo: "Estandar",
+            año: 2025,
+            precio: "$4,150,000",
+            icono: "🏎️"
+        }]
+    }, 
+    corvette: {
+        nombre: "Corvette",
+        modelos: [
+            {
+                nombre: "Stingray",
+                tipo: "Estándar",
+                año: 2025,
+                precio: "$6,555,000",
+                icono: "🔥"
+            },
+            {
+                nombre: "Z06",
+                tipo: "Automático",
+                año: 2025,
+                precio: "$5,870,000",
+                icono: "🚀"
+            }
+        ]
+    },
+    audi: {
+        nombre: "Audi",
+        modelos: [
+            {
+                nombre: "R8",
+                tipo: "Automático",
+                año: 2025,
+                precio: "$2,800,000",
+                icono: "🦾"
+            },
+            {
+                nombre: "e-tron GT",
+                tipo: "Eléctrico",
+                año: 2025,
+                precio: "$2,720,000",
+                icono: "🔋"
+            }
+        ]
+    }
+}
+function mostrarModelos(marcaSeleccionada) {
+    const contenedor = document.getElementById("modelos");
+    const marca = marcas[marcaSeleccionada];
+
+    contenedor.innerHTML = `<h2>Modelos de ${marca.nombre}</h2>`;
+
+    marca.modelos.forEach();
+}
